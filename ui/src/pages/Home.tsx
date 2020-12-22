a964b65af7cf34affc769cce16e1a86c75c6034d
scott-m-king <scott.m.king@outlook.com>
2020-12-22 02:16:57 -0800

    Finished prototype of text colourizer

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,12 +1,38 @@
 import React, { FC } from "react";
-import { InfoDrawer } from "../components/InfoDrawer";
+import { mainColour } from "../styles/palette";
+import { TextColourizer } from "../utils/TextColourizer";
 
-interface HomeProps {}
-
-export const Home: FC<HomeProps> = ({}) => {
+export const Home: FC = () => {
   return (
     <div>
       <h1>Home</h1>
+      <TextColourizer
+        text="Reduces the target's armor and magic resistance by 0.5, stacking up to 100 times for a total of 50 maximum resistances reduction. Lasts for 8s, reduced to 4.5s after Baron Nashor is slain"
+        paletteSet={{
+          purple: mainColour.purple,
+          green: mainColour.green,
+          orange: mainColour.orange,
+        }}
+        colourSet={{
+          purple: ["Baron Nashor"],
+          green: ["resistance", "0.5", "100", "50", "8s", "4.5s"],
+          orange: ["armor"],
+        }}
+      />
+      <br />
+      <TextColourizer
+        text="Baron Nashor summons a tremor beneath the target's location that erupts with a tentacle after 1.25s, dealing 25% AD magic damage to units within and knocking them up for a duration based on their proximity to the area's center. Units near the area's edge are also slightly knocked away."
+        paletteSet={{
+          purple: mainColour.purple,
+          green: mainColour.green,
+          orange: mainColour.orange,
+        }}
+        colourSet={{
+          purple: ["Baron Nashor"],
+          green: ["1.25s", "magic damage"],
+          orange: ["25% AD"],
+        }}
+      />
     </div>
   );
 };
---
+++ ui/src/utils/TextColourizer.tsx
@@ -0,0 +1,89 @@
+import Typography from "@material-ui/core/Typography";
+import React, { FC, useEffect, useState } from "react";
+import { findKey } from "lodash";
+
+export interface ColourSetType {
+  [key: string]: Array<string>;
+}
+
+export interface PositionType {
+  start: number;
+  end: number;
+}
+
+export interface IndexSetType {
+  [key: string]: Array<PositionType>;
+}
+
+export interface PaletteType {
+  [key: string]: string;
+}
+
+export interface TextColourizerProps {
+  text: string;
+  colourSet: ColourSetType;
+  paletteSet: PaletteType;
+}
+
+export const TextColourizer: FC<TextColourizerProps> = ({
+  text,
+  colourSet,
+  paletteSet,
+}) => {
+  const [final, setFinal] = useState<Array<JSX.Element>>([]);
+
+  useEffect(() => {
+    const indexSet: IndexSetType = {};
+    let set = new Set();
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#Finding_all_the_occurrences_of_an_element
+    Object.keys(colourSet).forEach((colour) => {
+      colourSet[colour].forEach((phrase) => {
+        let indices = [];
+        let idx = text.indexOf(phrase);
+        while (idx !== -1) {
+          set.add(idx);
+          indices.push({
+            start: idx,
+            end: idx + phrase.length,
+          });
+          idx = text.indexOf(phrase, idx + 1);
+        }
+        if (indexSet[colour]) {
+          indexSet[colour] = [...indexSet[colour], ...indices];
+        } else {
+          indexSet[colour] = indices;
+        }
+      });
+    });
+
+    let result: Array<JSX.Element> = [];
+
+    for (let i = 0; i < text.length; ) {
+      let curr = [];
+      while (!set.has(i)) {
+        curr.push(text[i]);
+        i++;
+        if (i >= text.length) {
+          result.push(<span>{curr.join("")}</span>);
+          return;
+        }
+      }
+
+      result.push(<span>{curr.join("")}</span>);
+
+      const colour = findKey(indexSet, (e) => e.some((x) => x.start === i));
+      const phrase = indexSet[colour!].find((e) => e.start === i);
+
+      result.push(
+        <span style={{ color: paletteSet[colour!] }}>
+          {text.substring(phrase!.start, phrase!.end)}
+        </span>
+      );
+      i = phrase!.end;
+      setFinal(result);
+    }
+  }, [colourSet, paletteSet, text]);
+
+  return <Typography>{final.map((elem) => elem)}</Typography>;
+};
